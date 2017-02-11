42f0ecf3a2bf9ceb423237cc928bd02f850f7378
m1t0k <yuriy.matveychuk@gmail.com>
2017-02-11 23:21:18 +0300

    ....

--- api/infrastructure/v1/gin/controllers/public_api.go
+++ api/infrastructure/v1/gin/controllers/public_api.go
@@ -5,21 +5,32 @@ import (
 
 	"github.com/gin-gonic/gin"
 	dicProvider "github.com/m1t0k/dictionaryService/api/dictionary/v1/business"
-	"github.com/m1t0k/dictionaryService/api/infrastructure/v1/config"
 	middleware "github.com/m1t0k/dictionaryService/api/infrastructure/v1/gin/middleware"
 )
 
 // DictionaryController implements http logic over DictionaryProvider
 type DictionaryController struct {
 	dicProvider dicProvider.IDictionaryPublicProvider
-}:{}
+}
 
 // CreateDictionaryController instance of DictionaryController
-func CreateDictionaryController(settings config.Configuration) *DictionaryController {
+/*func CreateDictionaryController(settings config.Configuration) *DictionaryController {
 	var controller DictionaryController
 	controller.dicProvider = dicProvider.CreateDictionaryProvider(settings.DbServer, settings.DbName)
 
-	log.Println("CreateDictionaryController:"+controller.dicProvider != nil)
+	//log.Println("CreateDictionaryController:"+controller.dicProvider != nil)
+	return &controller
+}*/
+
+func CreateDictionaryController(dbServer string, dbName string) *DictionaryController {
+	log.Println("CreateDictionaryController:>>>")
+
+	var controller DictionaryController
+	log.Println("CreateDictionaryController:>>> creating provider")
+	controller.dicProvider = dicProvider.CreateDictionaryProvider(dbServer, dbName)
+	log.Println("CreateDictionaryController:>>> provider created")
+
+	log.Println("CreateDictionaryController:<<<")
 	return &controller
 }
 
@@ -43,6 +54,12 @@ func (controller *DictionaryController) GetDictionaryItem(context *gin.Context) 
 
 // RegisterEx DictionaryController in router
 func RegisterDictionaryController(router *gin.Engine) {
-	router.GET("/dics/:dicCode", GetDictionaryItems)
-	router.GET("/dics/:dicCode/:code",GetDictionaryItem)
+	router.GET("/dics/:dicCode", func(c *gin.Context) {
+		controller := CreateDictionaryController("localhost", "dictionarydb")
+		controller.GetDictionaryItems(c)
+	})
+	router.GET("/dics/:dicCode/:code", func(c *gin.Context) {
+		controller := CreateDictionaryController("localhost", "dictionarydb")
+		controller.GetDictionaryItem(c)
+	})
 }
